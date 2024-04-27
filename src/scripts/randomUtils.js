91deab6944659aa957d52d8b718dba72d848fe5b
JAYANT SINGH <97082996+jayantsB123@users.noreply.github.com>
2024-04-27 14:15:22 +0530

    Update randomUtils.js

--- src/scripts/randomUtils.js
+++ src/scripts/randomUtils.js
@@ -1,4 +1,5 @@
 const randomIntFromRange = (min, max) => {
+  
   const minNorm = Math.ceil(min);
   const maxNorm = Math.floor(max);
   const idx = Math.floor(Math.random() * (maxNorm - minNorm + 1) + minNorm);
