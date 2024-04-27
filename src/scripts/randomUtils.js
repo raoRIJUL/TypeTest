440d732aebc8f890cae33a39167a9d1f5148ddaa
JAYANT SINGH <97082996+jayantsB123@users.noreply.github.com>
2024-04-27 14:15:49 +0530

    Update randomUtils.js

--- src/scripts/randomUtils.js
+++ src/scripts/randomUtils.js
@@ -5,6 +5,7 @@ const randomIntFromRange = (min, max) => {
   const maxNorm = Math.floor(max);
   
   const idx = Math.floor(Math.random() * (maxNorm - minNorm + 1) + minNorm);
+  
   return idx;
 };
 export { randomIntFromRange };
